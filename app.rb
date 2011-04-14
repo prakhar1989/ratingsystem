5512d01bcaa23628eb64cebe265fbc8744398e1f
Prakhar Srivastav <prakhar1989@gmail.com>
2011-04-14 12:25:18 +0530

    Ajax with Charts

--- app.rb
+++ app.rb
@@ -1,11 +1,24 @@
-['rubygems', 'sinatra', 'slim'].each{|r| require r}
+['rubygems', 'sinatra', 'slim', 'CSV'].each{|r| require r}
 
 set :slim, :pretty => true
+
+
+mystring = "12,35,27,21,78"
 
 get '/' do
   slim :index
 end
 
-get '/rate' do
-  "registered"
+post '/rate' do
+    myLine = File.open('stored_ratings.txt','r').readline
+    myArray = myLine.split(/,/)
+    index = params[:rating].to_i-1
+    myArray[index] = (myArray[index].to_i + 1).to_s
+    myLine = myArray.join(',')
+    File.open('stored_ratings.txt','w') do |f2|
+      f2.puts myLine
+    end  
+    
+    myLine
 end
+
